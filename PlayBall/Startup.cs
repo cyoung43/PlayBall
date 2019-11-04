a6f41795d650d607d9765d6539730b7217fd3d11
cyoung43 <55853741+cyoung43@users.noreply.github.com>
2019-11-04 07:43:41 -0700

    Add project files.

---
+++ PlayBall/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PlayBall.Startup))]
+namespace PlayBall
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
